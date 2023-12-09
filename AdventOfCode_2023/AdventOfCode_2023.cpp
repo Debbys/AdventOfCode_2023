7ca5586877c47797cb1b8ac3ec2500984d4d86b8
Debbys <d.foell@gmx.net>
2023-12-09 17:13:33 +0100

    Level 2 of the day 1 challenge

--- AdventOfCode_2023/AdventOfCode_2023.cpp
+++ AdventOfCode_2023/AdventOfCode_2023.cpp
@@ -7,21 +7,26 @@ using namespace std;
 
 uint8_t main()
 {
+	AdventOfCode_2023 advent = AdventOfCode_2023();
+
 	cout << "Press enter to get the result for the first Level of the day1 Challenge." << endl;
 	system("pause");
 
-	AdventOfCode_2023 advent = AdventOfCode_2023();
-	uint16_t result = advent.getResultForDay1ChallengeLevel1();
-
+	uint16_t result = advent.getResultForDay1Challenge(false);
 	cout << "final result: " << result << endl;
 
+	cout << "Press enter to get the result for the second Level of the day1 Challenge." << endl;
+	system("pause");
+
+	uint16_t result2 = advent.getResultForDay1Challenge(true);
+	cout << "final result: " << result2 << endl;
 	system("pause");
 
 	return 0;
 }
 
 
-uint16_t AdventOfCode_2023::getResultForDay1ChallengeLevel1(void)
+uint16_t AdventOfCode_2023::getResultForDay1Challenge(bool withWords)
 {
 	uint16_t accumulatedResult = 0U;
 
@@ -36,7 +41,7 @@ uint16_t AdventOfCode_2023::getResultForDay1ChallengeLevel1(void)
 		while (day1Input)
 		{
 			day1Input.getline(line, 100);
-			accumulatedResult += getNumberResultForLine(line);
+			accumulatedResult += getNumberResultForLine(line, withWords);
 		}
 		day1Input.close();
 	}
@@ -48,15 +53,15 @@ uint16_t AdventOfCode_2023::getResultForDay1ChallengeLevel1(void)
 	return accumulatedResult;
 }
 
-uint8_t AdventOfCode_2023::getNumberResultForLine(string line)
+uint8_t AdventOfCode_2023::getNumberResultForLine(string line, bool withWords)
 {
-	uint8_t firstDigit	= getFirstDigit(line);
-	uint8_t lastDigit	= getLastDigit(line);
+	uint8_t firstDigit	= getFirstDigit(line, withWords);
+	uint8_t lastDigit	= getLastDigit(line, withWords);
 
 	return (firstDigit * 10U) + lastDigit;
 }
 
-uint8_t AdventOfCode_2023::getFirstDigit(string line)
+uint8_t AdventOfCode_2023::getFirstDigitInString(string line)
 {
 	regex re("\\d");
 	smatch digits;
@@ -65,33 +70,81 @@ uint8_t AdventOfCode_2023::getFirstDigit(string line)
 
 	if (digits.size() != 0)
 	{
-		return (uint8_t) stoi(digits[0].str()); //since we know that the match will always be a digit
+		return (uint8_t)stoi(digits[0].str()); //since we know that the match will always be a digit
 	}
 	else
 	{
 		return 0;
 	}
-
-	
 }
 
-uint8_t AdventOfCode_2023::getLastDigit(string line)
+uint8_t AdventOfCode_2023::getFirstDigit(string line, bool withWords)
 {
-	regex re("\\d");
-	smatch digits;
-	string revline = line;
+	string convertedLine = line;
+
+	// if spelled out words need to be considered,
+	// convert them into actual digits before searching the string
+	if (withWords) 
+	{
+		regex re("\(one\|two\|three\|four\|five\|six\|seven\|eight\|nine\)");
+		smatch digits;
+
+		regex_search(line, digits, re);
+
+		string replaceValue;
+
+		// replace the first word digit in the string if there is one
+		if (digits.size() != 0)
+		{
+			for (uint8_t i = 0U; i < 9U; i++)
+			{
+				if (digits[0].str() == wordDigits[i])
+				{
+					replaceValue = actualDigits[i];
+					break;
+				}
+			}
+
+			convertedLine = regex_replace(convertedLine, regex(digits[0].str()), replaceValue);
+		}
+	}
+
+	return getFirstDigitInString(convertedLine);
+}
+
+uint8_t AdventOfCode_2023::getLastDigit(string line, bool withWords)
+{
+	string convertedLine = line;
 
 	// reverse the input string to search from back to front
-	reverse(revline.begin(), revline.end());
+	reverse(convertedLine.begin(), convertedLine.end());
 
-	regex_search(revline, digits, re);
+	// if spelled out words need to be considered,
+	// convert them into actual digits before searching the string
+	if (withWords)
+	{
+		regex reverseRe("\(enin\|thgie\|neves\|xis\|evif\|ruof\|eerht\|owt\|eno\)");
+		smatch revDigits;
 
-	if (digits.size() != 0)
-	{
-		return (uint8_t) stoi(digits[0].str()); //since we know that the match will always be a digit
+		regex_search(convertedLine, revDigits, reverseRe);
+
+		string replaceValue;
+
+		// replace the last word digit in the string if there is one
+		if (revDigits.size() != 0)
+		{
+			for (uint8_t i = 0U; i < 9U; i++)
+			{
+				if (revDigits[0].str() == revWordDigits[i])
+				{
+					replaceValue = actualDigits[i];
+					break;
+				}
+			}
+
+			convertedLine = regex_replace(convertedLine, regex(revDigits[0].str()), replaceValue);
+		}
 	}
-	else
-	{
-		return 0;
-	}
+
+	return getFirstDigitInString(convertedLine);
 }
--- AdventOfCode_2023/AdventOfCode_2023.h
+++ AdventOfCode_2023/AdventOfCode_2023.h
@@ -18,36 +18,54 @@ class AdventOfCode_2023
 public:
 	AdventOfCode_2023() {};
 
-	~AdventOfCode_2023() {};
-
+	~AdventOfCode_2023() {}; 
+	
 	/// <summary>
 	/// solve the task of getting the first and the last digit out of every line of day1Input.txt
 	/// to build the 2-digit numbers and add them up to the final value
 	/// </summary>
-	/// <param name=""></param>
+	/// <param name="withWords">shall spelled out digits also be considered?</param>
 	/// <returns>final accumulated number</returns>
-	uint16_t getResultForDay1ChallengeLevel1(void);	
+	uint16_t getResultForDay1Challenge(bool withWords);	
+
 
 private:
+
+	string wordDigits[9] = { "one", "two", "three" , "four", "five", "six", "seven", "eight", "nine" };
+	string revWordDigits[9] = { "eno", "owt", "eerht", "ruof", "evif", "xis", "neves", "thgie", "enin" };
+	string actualDigits[9] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 	/// <summary>
 	/// get the 2-digit number for the requested string
 	/// </summary>
 	/// <param name="line">one line of the day1Input.txt</param>
+	/// <param name="withWords">shall spelled out digits also be considered?</param>
 	/// <returns>2-digit number consisting out of the first and the last digit contained in the file</returns>
-	uint8_t getNumberResultForLine(string line);
+	uint8_t getNumberResultForLine(string line, bool withWords);
+
+	/// <summary>
+	/// returns the first digit in the provided line,
+	/// depending on the bool input spelled out digits will also be considered
+	/// </summary>
+	/// <param name="line">line that shall be searched for digits</param>
+	/// <param name="withWords">shall spelled out digits also be considered?</param>
+	/// <returns>the first digit in the string</returns>
+	uint8_t getFirstDigit(string line, bool withWords);
+
+	/// <summary>
+	/// returns the last digit in the provided line,
+	/// depending on the bool input spelled out digits will also be considered
+	/// </summary>
+	/// <param name="line">line that shall be searched for digits</param>
+	/// <param name="withWords">shall spelled out digits also be considered</param>
+	/// <returns>the last digit in the string</returns>
+	uint8_t getLastDigit(string line, bool withWords);
 
 	/// <summary>
 	/// get the first digit that is contained in a given string
 	/// </summary>
 	/// <param name="line">given string</param>
 	/// <returns>the first digit in the string</returns>
-	uint8_t getFirstDigit(string line);
+	uint8_t getFirstDigitInString(string line);
 
-	/// <summary>
-	/// get the last digit that is contained in a given string
-	/// </summary>
-	/// <param name="line">given string</param>
-	/// <returns>the last digit in the string</returns>
-	uint8_t getLastDigit(string line);
 };
