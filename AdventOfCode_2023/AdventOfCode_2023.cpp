b9685ba5a68cc901f58c460f5c3d285052dd2ce6
Debbys <d.foell@gmx.net>
2023-12-09 14:58:06 +0100

    Day1 Challenge1

---
+++ AdventOfCode_2023/AdventOfCode_2023.cpp
@@ -0,0 +1,97 @@
+﻿// AdventOfCode_2023.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "AdventOfCode_2023.h"
+
+using namespace std;
+
+uint8_t main()
+{
+	cout << "Press enter to get the result for the first Level of the day1 Challenge." << endl;
+	system("pause");
+
+	AdventOfCode_2023 advent = AdventOfCode_2023();
+	uint16_t result = advent.getResultForDay1ChallengeLevel1();
+
+	cout << "final result: " << result << endl;
+
+	system("pause");
+
+	return 0;
+}
+
+
+uint16_t AdventOfCode_2023::getResultForDay1ChallengeLevel1(void)
+{
+	uint16_t accumulatedResult = 0U;
+
+	//read in the input for the Day1 Challenge
+	ifstream day1Input;
+	day1Input.open("assets\\day1Input.txt");
+
+	char line[100];
+	if (day1Input.is_open())
+	{
+		// get the 2-digit number out of each line and add the value to the result
+		while (day1Input)
+		{
+			day1Input.getline(line, 100);
+			accumulatedResult += getNumberResultForLine(line);
+		}
+		day1Input.close();
+	}
+	else
+	{
+		cout << "could not open file" << endl;
+	}
+
+	return accumulatedResult;
+}
+
+uint8_t AdventOfCode_2023::getNumberResultForLine(string line)
+{
+	uint8_t firstDigit	= getFirstDigit(line);
+	uint8_t lastDigit	= getLastDigit(line);
+
+	return (firstDigit * 10U) + lastDigit;
+}
+
+uint8_t AdventOfCode_2023::getFirstDigit(string line)
+{
+	regex re("\\d");
+	smatch digits;
+
+	regex_search(line, digits, re);
+
+	if (digits.size() != 0)
+	{
+		return (uint8_t) stoi(digits[0].str()); //since we know that the match will always be a digit
+	}
+	else
+	{
+		return 0;
+	}
+
+	
+}
+
+uint8_t AdventOfCode_2023::getLastDigit(string line)
+{
+	regex re("\\d");
+	smatch digits;
+	string revline = line;
+
+	// reverse the input string to search from back to front
+	reverse(revline.begin(), revline.end());
+
+	regex_search(revline, digits, re);
+
+	if (digits.size() != 0)
+	{
+		return (uint8_t) stoi(digits[0].str()); //since we know that the match will always be a digit
+	}
+	else
+	{
+		return 0;
+	}
+}
---
+++ AdventOfCode_2023/AdventOfCode_2023.h
@@ -0,0 +1,53 @@
+﻿// AdventOfCode_2023.h: Header for the main class used to solve the AdventOfCode_2023 tasks
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+/// <summary>
+/// Main Class to collect all the functions needed to solve the AdventOfCode_2023 challenges
+/// </summary>
+class AdventOfCode_2023
+{
+
+public:
+	AdventOfCode_2023() {};
+
+	~AdventOfCode_2023() {};
+
+	/// <summary>
+	/// solve the task of getting the first and the last digit out of every line of day1Input.txt
+	/// to build the 2-digit numbers and add them up to the final value
+	/// </summary>
+	/// <param name=""></param>
+	/// <returns>final accumulated number</returns>
+	uint16_t getResultForDay1ChallengeLevel1(void);	
+
+private:
+
+	/// <summary>
+	/// get the 2-digit number for the requested string
+	/// </summary>
+	/// <param name="line">one line of the day1Input.txt</param>
+	/// <returns>2-digit number consisting out of the first and the last digit contained in the file</returns>
+	uint8_t getNumberResultForLine(string line);
+
+	/// <summary>
+	/// get the first digit that is contained in a given string
+	/// </summary>
+	/// <param name="line">given string</param>
+	/// <returns>the first digit in the string</returns>
+	uint8_t getFirstDigit(string line);
+
+	/// <summary>
+	/// get the last digit that is contained in a given string
+	/// </summary>
+	/// <param name="line">given string</param>
+	/// <returns>the last digit in the string</returns>
+	uint8_t getLastDigit(string line);
+};
